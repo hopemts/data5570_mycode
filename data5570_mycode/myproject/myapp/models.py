4a5fa23e828bf19770b472ae8b232815b6c6d8a8
root <root@ip-172-31-14-88.us-west-1.compute.internal>
2025-02-03 23:48:27 +0000

    Push

--- data5570_mycode/myproject/myapp/models.py
+++ data5570_mycode/myproject/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    bacb_number = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return str(self.user)
